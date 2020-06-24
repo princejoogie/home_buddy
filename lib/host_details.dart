d14574c547c933aa87ce7a05e3f9b16481ac2dad
princejoogie <princejoogie@gmail.com>
2020-06-24 16:22:54 +0800

    host details

--- lib/host_details.dart
+++ lib/host_details.dart
@@ -1,26 +1,26 @@
-String baseUrl = 'http://smdccondosaleonline.com/home_buddy/images/';
-String loginAPI = 'http://smdccondosaleonline.com/home_buddy/api/login.php';
+String baseUrl = 'https://smdccondosaleonline.com/home_buddy/images/';
+String loginAPI = 'https://smdccondosaleonline.com/home_buddy/api/login.php';
 String registerAPI =
-    'http://smdccondosaleonline.com/home_buddy/api/register.php';
+    'https://smdccondosaleonline.com/home_buddy/api/register.php';
 String addToCartAPI =
-    'http://smdccondosaleonline.com/home_buddy/api/add_to_cart.php';
+    'https://smdccondosaleonline.com/home_buddy/api/add_to_cart.php';
 String removeFromCartAPI =
-    'http://smdccondosaleonline.com/home_buddy/api/remove_from_cart.php';
+    'https://smdccondosaleonline.com/home_buddy/api/remove_from_cart.php';
 String getCartAPI =
-    'http://smdccondosaleonline.com/home_buddy/api/get_cart.php';
+    'https://smdccondosaleonline.com/home_buddy/api/get_cart.php';
 String getByCategoryAPI =
-    'http://smdccondosaleonline.com/home_buddy/api/get_category.php?category=';
+    'https://smdccondosaleonline.com/home_buddy/api/get_category.php?category=';
 String searchAPI =
-    'http://smdccondosaleonline.com/home_buddy/api/search.php?name=';
+    'https://smdccondosaleonline.com/home_buddy/api/search.php?name=';
 String addDeliveryAPI =
-    'http://smdccondosaleonline.com/home_buddy/api/add_delivery.php';
+    'https://smdccondosaleonline.com/home_buddy/api/add_delivery.php';
 String clearCartAPI =
-    'http://smdccondosaleonline.com/home_buddy/api/clear_cart.php';
+    'https://smdccondosaleonline.com/home_buddy/api/clear_cart.php';
 String getAlertsAPI =
-    'http://smdccondosaleonline.com/home_buddy/api/get_alerts.php';
+    'https://smdccondosaleonline.com/home_buddy/api/get_alerts.php';
 String deleteOrderAPI =
-    'http://smdccondosaleonline.com/home_buddy/api/delete_order.php';
+    'https://smdccondosaleonline.com/home_buddy/api/delete_order.php';
 String removeStockAPI =
-    'http://smdccondosaleonline.com/home_buddy/api/remove_stock.php';
+    'https://smdccondosaleonline.com/home_buddy/api/remove_stock.php';
 String getStockAPI =
-    'http://smdccondosaleonline.com/home_buddy/api/get_stock.php';
+    'https://smdccondosaleonline.com/home_buddy/api/get_stock.php';
