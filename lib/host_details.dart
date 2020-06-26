ddd1d6cd2c38a851007303f36d582699a13bd20f
princejoogie <princejoogie@gmail.com>
2020-06-27 01:43:39 +0800

    host details

--- lib/host_details.dart
+++ lib/host_details.dart
@@ -1,26 +1,16 @@
-String baseUrl = 'https://smdccondosaleonline.com/home_buddy/images/';
-String loginAPI = 'https://smdccondosaleonline.com/home_buddy/api/login.php';
-String registerAPI =
-    'https://smdccondosaleonline.com/home_buddy/api/register.php';
-String addToCartAPI =
-    'https://smdccondosaleonline.com/home_buddy/api/add_to_cart.php';
+String baseUrl = 'http://bylaloo.com/home_buddy/images/';
+String loginAPI = 'http://bylaloo.com/home_buddy/api/login.php';
+String registerAPI = 'http://bylaloo.com/home_buddy/api/register.php';
+String addToCartAPI = 'http://bylaloo.com/home_buddy/api/add_to_cart.php';
 String removeFromCartAPI =
-    'https://smdccondosaleonline.com/home_buddy/api/remove_from_cart.php';
-String getCartAPI =
-    'https://smdccondosaleonline.com/home_buddy/api/get_cart.php';
+    'http://bylaloo.com/home_buddy/api/remove_from_cart.php';
+String getCartAPI = 'http://bylaloo.com/home_buddy/api/get_cart.php';
 String getByCategoryAPI =
-    'https://smdccondosaleonline.com/home_buddy/api/get_category.php?category=';
-String searchAPI =
-    'https://smdccondosaleonline.com/home_buddy/api/search.php?name=';
-String addDeliveryAPI =
-    'https://smdccondosaleonline.com/home_buddy/api/add_delivery.php';
-String clearCartAPI =
-    'https://smdccondosaleonline.com/home_buddy/api/clear_cart.php';
-String getAlertsAPI =
-    'https://smdccondosaleonline.com/home_buddy/api/get_alerts.php';
-String deleteOrderAPI =
-    'https://smdccondosaleonline.com/home_buddy/api/delete_order.php';
-String removeStockAPI =
-    'https://smdccondosaleonline.com/home_buddy/api/remove_stock.php';
-String getStockAPI =
-    'https://smdccondosaleonline.com/home_buddy/api/get_stock.php';
+    'http://bylaloo.com/home_buddy/api/get_category.php?category=';
+String searchAPI = 'http://bylaloo.com/home_buddy/api/search.php?name=';
+String addDeliveryAPI = 'http://bylaloo.com/home_buddy/api/add_delivery.php';
+String clearCartAPI = 'http://bylaloo.com/home_buddy/api/clear_cart.php';
+String getAlertsAPI = 'http://bylaloo.com/home_buddy/api/get_alerts.php';
+String deleteOrderAPI = 'http://bylaloo.com/home_buddy/api/delete_order.php';
+String removeStockAPI = 'http://bylaloo.com/home_buddy/api/remove_stock.php';
+String getStockAPI = 'http://bylaloo.com/home_buddy/api/get_stock.php';
